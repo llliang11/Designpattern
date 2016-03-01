fac6cfd57cbea6ecc12d41c8f211c64e7b26217c
llliang11 <llliang11@163.com>
2016-03-01 20:41:50 +0800

    add decorator file
    example of decorator

---
+++ Decorator/Main.cpp
@@ -0,0 +1,92 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2016Äê2ÔÂ29ÈÕ
+ *      Author: larry
+ */
+#include <stdio.h>
+
+class File
+{
+public:
+	virtual void dataProcess(void *pOutData, int *outSize)
+	{
+		printf("file process data ...");
+	}
+};
+
+class DataProcess: public File
+{
+protected:
+	File *component;
+public:
+	void setProcessComponent(File *pFile)
+	{
+		component = pFile;
+	}
+
+	virtual void dataProcess(void *pOutData, int *outSize)
+	{
+		if (component != NULL)
+		{
+			component->dataProcess(NULL, NULL);
+		}
+	}
+};
+
+class Demux: public DataProcess
+{
+public:
+	virtual void dataProcess(void *pOutData, int *outSize)
+	{
+		DataProcess::dataProcess(NULL, NULL);
+		printf("Demux process data ...");
+	}
+};
+
+class Codec: public DataProcess
+{
+public:
+	virtual void dataProcess(void *pOutData, int *outSize)
+	{
+		DataProcess::dataProcess(NULL, NULL);
+		printf("Codec process data ...");
+	}
+};
+
+class Sink: public DataProcess
+{
+public:
+	virtual void dataProcess(void *pOutData, int *outSize)
+	{
+		DataProcess::dataProcess(NULL, NULL);
+		printf("Sink process data ...");
+	}
+};
+
+class Component: public DataProcess
+{
+public:
+	virtual void dataProcess(void *pOutData, int *outSize)
+	{
+		DataProcess::dataProcess(NULL, NULL);
+		printf("Component process data ...");
+	}
+};
+
+int main(void)
+{
+	File *aviFile = new File();
+	Demux *aviDemux = new Demux();
+	Codec *h264 = new Codec();
+	Sink *imageSink = new Sink();
+
+	aviDemux->setProcessComponent(aviFile);
+	h264->setProcessComponent(aviDemux);
+	imageSink->setProcessComponent(h264);
+	imageSink->dataProcess(NULL, NULL);
+	return 0;
+}
+
+
+
