791fe87c77612616758234f8bdf5909da881b179
llliang11 <llliang11@163.com>
2016-03-01 21:20:41 +0800

    decorator
    format file with tab

--- Decorator/Main.cpp
+++ Decorator/Main.cpp
@@ -9,86 +9,87 @@
 class File
 {
 public:
-	virtual void dataProcess(void *pOutData, int *outSize)
-	{
-		printf("file process data ...\n");
-	}
+   virtual void dataProcess(void *pOutData, int *outSize)
+   {
+      printf("file process data ...\n");
+   }
 };
 
 class DataProcess: public File
 {
 protected:
-	File *component;
+   File *component;
 public:
-	void setProcessComponent(File *pFile)
-	{
-		component = pFile;
-	}
+   void setProcessComponent(File *pFile)
+   {
+      component = pFile;
+   }
 
-	virtual void dataProcess(void *pOutData, int *outSize)
-	{
-		if (component != NULL)
-		{
-			component->dataProcess(NULL, NULL);
-		}
-	}
+   virtual void dataProcess(void *pOutData, int *outSize)
+   {
+      if (component != NULL)
+      {
+         component->dataProcess(NULL, NULL);
+      }
+   }
 };
 
 class Demux: public DataProcess
 {
 public:
-	virtual void dataProcess(void *pOutData, int *outSize)
-	{
-		DataProcess::dataProcess(NULL, NULL);
-		printf("Demux process data ...\n");
-	}
+   virtual void dataProcess(void *pOutData, int *outSize)
+   {
+      DataProcess::dataProcess(NULL, NULL);
+      printf("Demux process data ...\n");
+   }
 };
 
 class Codec: public DataProcess
 {
 public:
-	virtual void dataProcess(void *pOutData, int *outSize)
-	{
-		DataProcess::dataProcess(NULL, NULL);
-		printf("Codec process data ...\n");
-	}
+   virtual void dataProcess(void *pOutData, int *outSize)
+   {
+      DataProcess::dataProcess(NULL, NULL);
+      printf("Codec process data ...\n");
+   }
 };
 
 class Sink: public DataProcess
 {
 public:
-	virtual void dataProcess(void *pOutData, int *outSize)
-	{
-		DataProcess::dataProcess(NULL, NULL);
-		printf("Sink process data ...\n");
-	}
+   virtual void dataProcess(void *pOutData, int *outSize)
+   {
+      DataProcess::dataProcess(NULL, NULL);
+      printf("Sink process data ...\n");
+   }
 };
 
 class Component: public DataProcess
 {
 public:
-	virtual void dataProcess(void *pOutData, int *outSize)
-	{
-		DataProcess::dataProcess(NULL, NULL);
-		printf("Component process data ...\n");
-	}
+   virtual void dataProcess(void *pOutData, int *outSize)
+   {
+      DataProcess::dataProcess(NULL, NULL);
+      printf("Component process data ...\n");
+   }
 };
 
 int main(void)
 {
-	File *aviFile = new File();
-	Demux *aviDemux = new Demux();
-	Codec *h264 = new Codec();
-	Sink *imageSink = new Sink();
+   File *aviFile = new File();
+   Demux *aviDemux = new Demux();
+   Codec *h264 = new Codec();
+   Sink *imageSink = new Sink();
 
-	Component *com = new Component();
+   Component *com = new Component();
 
-	aviDemux->setProcessComponent(aviFile);
-	h264->setProcessComponent(aviDemux);
-	com->setProcessComponent(h264);
-	imageSink->setProcessComponent(com);
-	imageSink->dataProcess(NULL, NULL);
-	return 0;
+   aviDemux->setProcessComponent(aviFile);
+   h264->setProcessComponent(aviDemux);
+   com->setProcessComponent(h264);
+   imageSink->setProcessComponent(com);
+   imageSink->dataProcess(NULL, NULL);
+
+   return 0;
 }
 
 
